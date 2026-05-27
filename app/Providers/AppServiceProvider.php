b6cfa8732551d073cdfac78314e90d5bb1997486
23sc4203ms-art <23sc4203ms-art@users.noreply.github.com>
2026-05-27 23:00:43 +0800

    Fix: remove deprecated HEADER_X_FORWARDED_ALL usage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,7 +5,6 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Http\Request;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,9 +25,6 @@ class AppServiceProvider extends ServiceProvider
 
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
-            
-            // Trust all proxies for reverse proxy situations (e.g., Render, cloud providers)
-            Request::setTrustedProxies(['*'], Request::HEADER_X_FORWARDED_ALL);
         }
     }
 }
